d7e829f369790d418a646885fdb14af980617bd3
Joe Kaiser <joe.kaiser@stackiq.com>
2017-05-09 15:27:26 -0600

    Version change for HDP 2.6 and Stacki 4.0

--- version.mk
+++ version.mk
@@ -1,8 +1,8 @@
 export ROLL		= stacki-hdp-bridge
 COLOR			= limegreen
 
-export VERSION		= 2.5
-export ROLLVERSION	= 2.5
+export VERSION		= 2.6
+export ROLLVERSION	= 2.6
 
 export CODENAME		= stacki
 export RELEASE		= 7.x
